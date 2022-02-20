ab8d8e70321c92cd796c3f6c0c6f95871ebfc8f0
yommyfaces <joeolumoye@yahoo.co.uk>
2022-02-20 15:08:31 +0000

    Changes to skills

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Software Tester", "Freelancer"],
+        strings: ["Developer", "Designer", "Software Tester", "Software Implementation Consultant"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Software Tester", "Freelancer"],
+        strings: ["Developer", "Designer", "Software Tester", "Software Implementation Consultant"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
